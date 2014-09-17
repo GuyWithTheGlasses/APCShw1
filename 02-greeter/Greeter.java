c21ea23187cdd6aa91d6b370df0eed7238c349de
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-09-16 22:40:07 -0400

    classwork from 9.14

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -1,7 +1,12 @@
+//import java.io.*;           why this no workkkk
+//import java.util.*;
+
+
 public class Greeter {
     public void greet() {
 	System.out.println("Hello world!");
     }
+    public void ungreet() {
+	System.out.println("I'm out.");
+    }
 }
-
-/* blah blah edit blah blah */
