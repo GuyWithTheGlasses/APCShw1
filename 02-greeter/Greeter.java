88c75402435fbea9d3a60843a86360011c837f70
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-09-21 12:47:41 -0400

    whee

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -1,48 +1,8 @@
-<<<<<<< HEAD
-
-import java.io.*;
-import java.util.*;
-=======
-//import java.io.*;           why this no workkkk
-//import java.util.*;
-
->>>>>>> ec878f9130e69b480820b3c79149b4a9e5321062
-
 public class Greeter {
-    // We put the attributes here
-
-    // We put the abilities here
-    // In Java, these are called methods.
-    // methods are functions but they belong
-    // to specific classes
-    
-    /*
-      public <- this method can be called (used) from
-                outside the class
-
-      void   <- this method doesn't return (send back)
-                anything 
-
-      greet  <- The name of the method
-  
-      ()     <- Parameters would go here
-
-      { }    <- the actual method code goes here
-     */
     public void greet() {
 	System.out.println("Hello world!");
-	
-	ungreet();
     }
-<<<<<<< HEAD
-
-    private void ungreet() {
-	System.out.println("I'm out.");
-    }
-
-=======
     public void ungreet() {
-	System.out.println("I'm out.");
+	System.out.println("Cya!");
     }
->>>>>>> ec878f9130e69b480820b3c79149b4a9e5321062
 }
