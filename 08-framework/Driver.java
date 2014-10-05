f8feb074bd3a4bbf3633d1e2a156b3f7262fdb95
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-10-05 16:03:34 -0400

    Submitted hw08

---
+++ 08-framework/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+    public static void main(String[] args){
+	Framework f = new Framework();
+	System.out.println(f.frame(5,4));
+	System.out.println(f.frame(4,5));
+	System.out.println(f.stringSplosion("DROPIT"+"\n"));
+	System.out.println(f.stringSplosion("HAHAHA"+"\n"));
+	System.out.println(f.stringX("xx3x60xnoxscxoxxpexx"));
+	System.out.println(f.stringX("qxq"));
+    }
+}
