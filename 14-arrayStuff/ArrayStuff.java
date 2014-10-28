4b422e5d4f31952aab1b25b4d3100d044179731e
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-10-27 20:45:46 -0400

    Submitted hw14

---
+++ 14-arrayStuff/ArrayStuff.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class ArrayStuff{
+    
+    /*------------- Instance Variables --------------*/
+
+    private int[] a;
+    Random rand;
+
+    /*--------------- Constructors ----------------*/
+
+    public ArrayStuff(int x){
+	rand = new Random();
+	a = new int[x];
+	
+	for(int i = 0 ; i < a.length ; i++){
+	    a[i] = 75 + rand.nextInt(76);
+	}
+
+    }
+
+    public ArrayStuff(){
+	this(100);
+    }
+
+    /*----------------- Methods --------------------*/
+
+    public String toString(){
+	String s = "";
+	
+	for(int i = 0 ; i < a.length ; i++){
+	    s = s + a[i] + ", ";
+	}
+
+	return s;
+    }
+
+    public int find(int n){
+	int index = -1;
+
+	for(int i = 0 ; i < a.length ; i++){
+	    if(a[i] == n){
+		index = i;
+		i = a.length;
+	    }
+	}
+
+	return index;
+    }
+
+    public int maxVal(){
+	int max = 0;
+
+	for(int i = 0 ; i < a.length ; i++){
+	    if(a[i] > max){
+		max = a[i];
+	    }
+	}
+
+	return max;
+    }   
+
+}
+	
---
+++ 14-arrayStuff/Driver.java
@@ -0,0 +1,14 @@
+public class Driver{
+    
+    public static void main(String[] args){
+	
+	ArrayStuff as = new ArrayStuff();
+	
+	System.out.println(as.toString());
+	System.out.println(as.find(150));
+	System.out.println(as.maxVal());
+
+    }
+    
+}
+	
