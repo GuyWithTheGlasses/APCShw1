8985b36360cafe981db5c1a92fb7d63e4dedc1ec
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-10-29 21:30:56 -0400

    Submitted hw14b

--- 14-arrayStuff/CodingBat.java
+++ 14-arrayStuff/CodingBat.java
@@ -51,26 +51,25 @@ public class CodingBat{
 	    return false;
 	}
 	
-	//Not working right now
 	public int[] tenRun(int[] nums){
+	    boolean running = false;
+	    int mul10 = 0;
+	    
 	    for(int i = 0 ; i < nums.length ; i++){
-		while( ((nums[i] % 10) == 0) && (i < nums.length) ){
-		    int multiple10 = nums[i];
-		    i++;
-		    while(i < nums.length){
-			if( (nums[i] % 10) != 0){
-			    nums[i] = multiple10;
-			    i++;
-			}
-			else{
-			    break;
-			}
-		    }
-		    i++;
+		
+		if((nums[i] % 10) == 0){
+		    running = true;
+		    mul10 = nums[i];
 		}
+		if(running){
+		    nums[i] = mul10;
+		}
+		
 	    }
+	    
 	    return nums;
 	}
+		
 
 	public boolean tripleUp(int[] nums) {
 	    boolean yeshno = false;
@@ -84,9 +83,52 @@ public class CodingBat{
 	    
 	    return yeshno;
 	}
-		    
-		    
 
+	/*------------- Harder HW Problems --------------*/
+		    
+	public boolean canBalance(int[] nums) {
+	    int[] split = new int[nums.length];
+	    boolean yeshno = false;
+  
+	    int sum1 = 0;
+	    int sum2 = 0;
+	    
+	    for(int n = 0 ; n < nums.length ; n++){
+		sum2 = sum2 + nums[n];
+	    }
+  
+	    for(int i = 0 ; i < nums.length ; i++){
+		
+		split[i] = nums[i];
+		sum1 = sum1 + split[i];
+		sum2 = sum2 - nums[i];
+
+		if(sum1 == sum2){
+		    yeshno = true;
+		}
+		
+	    }
+    
+	    return yeshno;
+	}	    
+
+	/*
+	  Idea for seriesUp, doesn't work
+
+	  public int[] seriesUp(int n) {
+	  int[] result = new int[(n * (n + 1)) / 2];
+	  int current = 0;
+	  int adder = 1;
+	  while(current < n){
+	  for(int i = 0 ; i < current ; i++){
+	  result[i] = adder;
+	  adder++;
+	  }
+	  current++;
+	  }
+	  return result;
+	  }
+	*/
 
     }
 
