959b9b6cc6a64aed3b4c0454c0d3cdbfadbb129c
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-11-09 16:00:56 -0500

    Submitted hw15

---
+++ 15-sarray/Sarray.java
@@ -0,0 +1,89 @@
+public class Sarray {
+    int[] data;
+    int   last;
+
+    public Sarray() {
+	// set up the initial instance variables
+	
+        data = new int[10];
+	last = 0;
+    }
+
+    public boolean add(int i){
+	// adds an item to the end of the list, grow if needed
+        // returns true
+	
+	if(last != (data.length - 1)){
+	    data[last+1] = i;
+	}
+	else{
+	    int[] newdata = new int[data.length + 1];
+	    for(int j = 0 ; j < (newdata.length - 1) ; j++){
+		newdata[j] = data[j];
+	    }
+	    newdata[last+1] = i;
+	}
+	last++;
+	return true;
+    }
+
+    public void  add(int index, int i){
+	// adds item i  at index, shifting everything down as needed.
+        // also grows as needed 
+
+	if(last < (data.length - 1)){
+	    for(int j=(data.length-1) ; j>index ; j=j-1){
+		data[j] = data[j-1];
+	    }
+	    data[index] = i;
+	}
+	
+	else{
+	    int[] newdata = new int[data.length+1];
+	    for(int k = 0 ; k < newdata.length ; k++){
+		if(k<index){
+		    newdata[k] = data[k];
+		}
+		if(k=index){
+		    newdata[k] = i;
+		}
+		else{
+		    newdata[k] = data[k-1];
+		}
+	    }
+	}
+	
+    }
+
+    public int size() {
+	// returns the number of items in the list (not the array size)
+
+        int numelements = last + 1;
+	return numelements;
+    }
+
+    public int get(int index) {
+        // returns the item at location index of the list
+
+	int location = data[index];
+	return location;
+    }
+
+    public int set(int index, int i){
+        // sets the item at location index to value i
+        // returns the old value 
+	
+        int oldval = data[index];
+	data[index] = i;
+	return oldval;
+    }
+
+    public int remove(int index){
+        // removes the item at index 
+        // returns the old value
+	
+	int removedval = data[index];
+	data[index] = 0;
+	return removedval;
+    }
+}
