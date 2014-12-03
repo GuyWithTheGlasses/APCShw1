b4df8d3a1d5379e928de9978b3bfd226c14ec00e
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-12-02 22:54:54 -0500

    12.2

--- 18-wordSearch/Driver.java
+++ 18-wordSearch/Driver.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver{
     public static void main(String[] args){
 	WordSearch ws = new WordSearch(10,20);
@@ -9,11 +12,28 @@ public class Driver{
 	//ws.addWordAllD("CAKE", 14, 21, 6); //illegal overlap
 	//ws.addWordAllD("TEA", 15, 20, 8); //direction is not a valid int
 
-	System.out.println(ws.addWord("water"));
-	System.out.println(ws.addWord("soda"));
-	System.out.println(ws.addWord("cider"));
-	System.out.println(ws.addWord("juice"));
+	//System.out.println(ws.addWord("water"));
+	//System.out.println(ws.addWord("soda"));
+	//System.out.println(ws.addWord("cider"));
+	//System.out.println(ws.addWord("juice"));
+
+	ArrayList input = new ArrayList<String>();
+	input.add("water");
+	input.add("cola");
+	input.add("juice");
+	input.add("coffee");
+	input.add("tea");
+	input.add("seltzer");
+	input.add("shake");
+	input.add("latte");
+	input.add("cocoa");
+	input.add("sprite");
+	
+	ws.makeWordList(input);
+	
+	ws.buildPuzzle(10);
 	System.out.println(ws.toString());
+	System.out.println(ws.getWords());
     }
 }
 
--- 18-wordSearch/WordSearch.java
+++ 18-wordSearch/WordSearch.java
@@ -3,13 +3,12 @@ import java.io.*;
 
 public class WordSearch{
     private char[][] board;
-<<<<<<< HEAD
+    private char[][] key;
 
-=======
     private Random rnd = new Random();
-    private ArrayList<String> wordlist, words;
-    
->>>>>>> e852e0fc214bff2de86f7b6be6acf2e4c4217ab8
+    private ArrayList<String> wordlist = new ArrayList<String>();
+    private ArrayList<String> words = new ArrayList<String>();
+
     public WordSearch(int r, int c){
 	board = new char[r][c];
 	for(int i = 0 ; i < board.length ; i++){
@@ -34,32 +33,26 @@ public class WordSearch{
 	}
 	return s;
     }
-
-    public void check(String word, int row, int col, int direction){
-	
-	/* 
-	   Direction starts from 0 and rotates by 45 degrees 
-	   counterclockwise for each increase of 1. For example, 2 is the 
-	   upward direction, 5 is down and to the left, etc.
-	   
-	   ***This should no longer ever be an issue, as we now generate the
-	   direction int using a Random.
-	*/
-	try{
-	    if(direction < 0 || direction > 7){
-		throw new IllegalArgumentException();
-	    }
-	} catch (IllegalArgumentException e){
-	    System.out.println("Direction must be an int between 0 and 7, inclusive.");
-	    System.exit(0);
+    
+    /* Not using the database of words yet, so this is here
+      to allow manual input of words. */
+    
+    public void makeWordList(ArrayList<String> als){
+	for(int i = 0 ; i < als.size() ; i++){
+	    String s = (als.get(i)).toUpperCase();
+	    wordlist.add(s);
 	}
-	
+    }
+
+    /*--------------------- AddWord Methods ---------------------*/
+
+    public boolean check(String word, int row, int col, int direction){
 	/*
 	  Here we find the "total" length of the word; that is,
 	  the farthest the word extends in its specified direction.
 	  If that extends out of the board, then it's invalid.
 	*/
-	System.out.println();
+	//System.out.println(word+" from ("+row+","+col+") in direction "+direction);
 	int lencheck = word.length();
 	int hgtcheck = word.length();
 	//Direction checker, will use this a lot in different places
@@ -76,21 +69,22 @@ public class WordSearch{
 	    hgtcheck = row + hgtcheck;
 	}
 
-	//Checks to see if word overlaps at any point
+	try{
+	    //Error check to see if word extends out of board.
+	    if(lencheck < 0 || hgtcheck < 0 || 
+	       lencheck > board[row].length || hgtcheck > board.length){
+		throw new IndexOutOfBoundsException();
+	    }
+	} catch (IndexOutOfBoundsException e) {}
+	    
+
+	//Checks to see if word overlaps at any point	    
 	boolean overlap = false;
 	int r = row;
 	int c = col;
 	for(int i = 0 ; i < word.length() ; i++){
-<<<<<<< HEAD
-	    if( 
-	       (board[r][c] == '.') ||
-	       (board[r][c] == word.charAt(i)) 
-		){
-=======
 	    if(board[r][c] == word.charAt(i) ||
 	       board[r][c] == '.'){
-		System.out.println("Overlap is false");
->>>>>>> e852e0fc214bff2de86f7b6be6acf2e4c4217ab8
 		if(direction == 0 || direction == 7 || direction == 1){
 		    c++;
 		}
@@ -104,33 +98,12 @@ public class WordSearch{
 		    r++;
 		}
 	    }
-	    else{
-		System.out.println("Overlap is true");
-		overlap = true;
-		break;
-	    }
-	}       
-
-	try{	    	   
-	    //Error check to see if word extends out of board.
-	    if(lencheck < 0 || hgtcheck < 0 || 
-	       lencheck > board[row].length || hgtcheck > board.length){
-		throw new IndexOutOfBoundsException();
-	    }
-	    //Error check to see if word causes conflict with overlap.
-	    if(overlap){
-<<<<<<< HEAD
-		throw new IndexOutOfBoundsException();
-=======
+	    else{  
 		throw new IllegalArgumentException();
->>>>>>> e852e0fc214bff2de86f7b6be6acf2e4c4217ab8
-	    }    	    
-	} catch (IndexOutOfBoundsException e){
-	    
-	} catch (IllegalArgumentException e){}
-	    
-	
-    }	    
+	    }
+	}
+	return true;
+    }
 
     public void addWordAllD(String word, int row, int col, int direction){
 	/* 
@@ -138,24 +111,27 @@ public class WordSearch{
 	   counterclockwise for each increase of 1. For example, 2 is the 
 	   upward direction, 5 is down and to the left, etc.
 	*/
-	check(word, row, col, direction);
-	int r = row;
-	int c = col;
-	String w = word.toUpperCase();
-	for(int i = 0 ; i < word.length() ; i++){
-	    board[r][c] = word.charAt(i);
-	    if(direction == 0 || direction == 7 || direction == 1){
-		c++;
+	if(check(word,row,col,direction)){
+   
+	    int r = row;
+	    int c = col;
+	    String w = word.toUpperCase();
+	    for(int i = 0 ; i < word.length() ; i++){
+		board[r][c] = word.charAt(i);
+		if(direction == 0 || direction == 7 || direction == 1){
+		    c++;
+		}
+		if(direction == 2 || direction == 1 || direction == 3){
+		    r--;
+		}
+		if(direction == 4 || direction == 3 || direction == 5){
+		    c--;
+		}
+		if(direction == 6 || direction == 5 || direction == 7){
+		    r++;
+		}
 	    }
-	    if(direction == 2 || direction == 1 || direction == 3){
-		r--;
-	    }
-	    if(direction == 4 || direction == 3 || direction == 5){
-		c--;
-	    }
-	    if(direction == 6 || direction == 5 || direction == 7){
-		r++;
-	    }
+	    
 	}
     }
 
@@ -168,45 +144,18 @@ public class WordSearch{
 	try{
 	    addWordAllD(w, r, c, d);
 	}catch (IndexOutOfBoundsException e){
-<<<<<<< HEAD
-	    System.out.println("Failed to add word "+w.toUpperCase());
-=======
-	    System.out.println("Word "+w+" goes out of bounds.");
->>>>>>> e852e0fc214bff2de86f7b6be6acf2e4c4217ab8
+	    //System.out.println("Word "+w+" goes out of bounds.");
 	    return false;
 	}catch (IllegalArgumentException e){
-	    System.out.println("Word "+w+" overlaps improperly.");
+	    //System.out.println("Word "+w+" overlaps improperly.");
 	    return false;
 	}
-	System.out.println("Added word successfully.");
+	//System.out.println("Added word successfully.");
 	return true;  
 
     }
 
-    public void buildPuzzle(int numwords){
-	words = new ArrayList<String>();
-	int i = 0 ;
-	while(i < numwords){
-	    int wordIndex = rnd.nextInt(wordlist.size());
-	    String word = wordList.get(wordIndex);
-	    if(addWord(word)){
-		words.add(word);
-		wordlist.remove(wordIndex);
-		i++;
-	    }
-	}
-	makeKey();
-	
-	for(int i = 0 ; i < board.length ; i++){
-	    for(int j = 0 ; j < board[i].length ; j++){
-		if(board[i][j] == '.'){
-		    String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		    board[i][j] = letters.charAt(rnd.nextInt(letters.length()));
-		}
-	    }
-	}
-	
-    }
+    /*--------------------------- Macro Methods --------------------------*/
 
     public void makeKey(){
 	key = new char[board.length][board[0].length];
@@ -217,8 +166,32 @@ public class WordSearch{
 	}
     }
 
+    public void buildPuzzle(int numwords){ 
+	int i = 0 ;
+	while(i < numwords){
+	    int wordIndex = rnd.nextInt(wordlist.size());
+	    String word = wordlist.get(wordIndex);
+	    if(addWord(word)){
+		words.add(word);
+		wordlist.remove(wordIndex);
+		i++;
+	    }
+	}
+	makeKey();
+	
+	for(int j = 0 ; j < board.length ; j++){
+	    for(int k = 0 ; k < board[j].length ; k++){
+		if(board[j][k] == '.'){
+		    String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		    board[j][k] = letters.charAt(rnd.nextInt(letters.length()));
+		}
+	    }
+	}
+	
+    }
+
     public String getWords(){
 	return ""+words;
     }
-			   
+     
 }
